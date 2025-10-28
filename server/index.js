b680d641b4040a799598d668473914017b39cf35
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-10-29 00:18:39 +0530

    bug: fixed deploy error

--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ app.use(express.static(path.join(__dirname, "../client/dist")));
 app.get("/*splat", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
