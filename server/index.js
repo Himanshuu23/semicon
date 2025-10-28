376cd05bfb813613b8536f8e7aa509c28d0dc88a
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-10-29 00:15:04 +0530

    bug: build fix

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname, "../client/dist")));
-app.get("*", (req, res) => {
+app.get("/*splat", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 const PORT = process.env.PORT || 8080;
